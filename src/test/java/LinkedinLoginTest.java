b6f99dd10f22f51405be0e98460f62cc5e0cbac7
Kseniia Kozhushko <ksusha.kozhushko@gmail.com>
2018-05-24 21:56:24 +0300

    Lesson 8

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -29,7 +29,6 @@ public class LinkedinLoginTest {
                 { "ksko.ksko1996@gmail.com", "RECKLESS1" },
                 { "KSKO.KSKO1996@GMAIL.COM", "RECKLESS1" },
                 { "ksko.ksko@gmail.com", "reckless1" },
-
         };
     }
 
@@ -47,18 +46,16 @@ public class LinkedinLoginTest {
         LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(webDriver);
 
         Assert.assertEquals(linkedinLoginPage.getCurrentTitle(),"LinkedIn: Log In or Sign Up", "Login page Title is wrong");
-
         Assert.assertTrue(linkedinLoginPage.isSignInButtonDisplayed(), "Sign In Button is not displayed");
-        linkedinLoginPage.login(email, password);
 
-        LinkedinHomePage linkedinHomePage = new LinkedinHomePage(webDriver);
+        LinkedinHomePage linkedinHomePage = linkedinLoginPage.login(email, password);
+
         Assert.assertEquals(linkedinHomePage.getCurrentUrl(),"https://www.linkedin.com/feed/", "Home Page URL is wrong.");
         Assert.assertTrue(linkedinHomePage.getCurrentTitle().contains("LinkedIn"), "Home page Title is wrong" );
     }
 
     @Test(dataProvider = "invalidDataProvider")
     public void negativeReturnedToLoginSubmitTest(String email, String password) {
-
         LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(webDriver);
 
         Assert.assertEquals(linkedinLoginPage.getCurrentTitle(),"LinkedIn: Log In or Sign Up", "Login page Title is wrong");
@@ -74,7 +71,6 @@ public class LinkedinLoginTest {
 
     @Test(dataProvider = "emptyFieldsDataProvider")
     public void emptyFieldsLoginTest(String email, String password) {
-
         LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(webDriver);
 
         Assert.assertEquals(linkedinLoginPage.getCurrentTitle(),"LinkedIn: Log In or Sign Up", "Login page Title is wrong");
